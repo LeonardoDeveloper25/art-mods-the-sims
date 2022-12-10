88b0870525679e915d9d86b3c8f28606343884ff
LeonardoDeveloper25 <120230121+LeonardoDeveloper25@users.noreply.github.com>
2022-12-09 22:28:12 -0300

    versão 1

---
+++ menu.js
@@ -0,0 +1,24 @@
+function clickMenu(itens){
+    if (itens.style.display =='block'){
+         itens.style.display = 'none'
+ } else {
+     itens.style.display = 'block'
+    }
+}
+
+function clickMenu(itens2){
+    if (itens2.style.display =='block'){
+         itens2.style.display = 'none'
+ } else {
+     itens2.style.display = 'block'
+    }
+}
+
+function clickMenu(itens3){
+    if (itens3.style.display =='block'){
+         itens3.style.display = 'none'
+ } else {
+     itens3.style.display = 'block'
+    }
+}
+
